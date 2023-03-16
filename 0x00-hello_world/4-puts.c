b7887e5ee16db95cad7a2d6c3d0d098bf67d91e7
omorakwant <omasavalsys@gmail.com>
2023-03-16 08:04:10 -0700

    idk what am i doing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,4 +4,8 @@
  *   *
  *    * Return: Always 0 (Success)
  *     */
-int main(void){puts("\"Programming is like building a multilingual puzzle");}
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
