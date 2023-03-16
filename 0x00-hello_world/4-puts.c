fbc044ac1d624cc04a0a19b0ea3db3194fabdaf1
omorakwant <omasavalsys@gmail.com>
2023-03-16 08:02:07 -0700

    pls gooo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,4 @@
  *   *
  *    * Return: Always 0 (Success)
  *     */
-int main()
-{
-	        puts("\"Programming is like building a multilingual puzzle");
-		return (0);
-}
+int main(void){puts("\"Programming is like building a multilingual puzzle");}
