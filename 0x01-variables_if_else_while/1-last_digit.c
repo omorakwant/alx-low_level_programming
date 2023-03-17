8940d1e7cf725690eccd90265d3e0412e095eee3
omorakwant <omasavalsys@gmail.com>
2023-03-17 22:04:34 +0100

    3-print_alphabets.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,10 +11,8 @@
 int main(void)
 {
 int n;
-
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-
 if ((n % 10) > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n",n, n % 10);
@@ -28,5 +26,4 @@ else
 printf("Last digit of %d is %d and is 0\n",n, n % 10);
 }
 return (0);
-
 }
